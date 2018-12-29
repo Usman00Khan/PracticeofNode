00b72031432f95ea70f922570fb57d8e766b0b08
Usman Khan <uk80718@gmail.com>
2018-12-29 11:33:31 +0530

    My first package

---
+++ King-Lib/index.js
@@ -0,0 +1,3 @@
+module.exports.add = function( a, b){return (a + b)}
+module.exports.multiply = function( a, b){return(a*b)}
+module.exports.sub = function(a,b){return(b - a)}
---
+++ nodeapp/index.js
@@ -0,0 +1,3 @@
+var king = require("king-lib-utg")
+var r = king.add(2,3)
+console.log(r)
