cf8dd66819c6ec649f96a1516e0c68af89e7af83
Usman Khan <uk80718@gmail.com>
2018-12-30 23:37:05 +0530

    added delete and modify feature using PUT and DELETE request

--- Express_demo/index.js
+++ Express_demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 app.use(express.json())
@@ -17,22 +18,62 @@ app.get('/api/courses/',(req,res) => {
 
 app.get('/api/courses/:id',(req,res)=>{
     const course = courses.find(p => p.id === parseInt(req.params.id))
-    if(course){
-        res.    send(course)
+    if(!course){
+        return res.status(404).send('Course not found!')
     }
-    else{
-        res.status(404).send('Course not found!')
-    }
+    res.send(course)
 })
 
 app.post('/api/courses',(req,res)=>{
+    const {error} = validateCoure(req.body)
+    if(error) {
+        res.status(400).send(error.details[0].message)
+        return
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
     }
     courses.push(course)
-    res.send(course)
+    res.status(201).send(course)
 })
+
+app.put('/api/courses/:id',(req,res) => {
+
+    const course = courses.find(p => p.id === parseInt(req.params.id))
+    if(!course){
+        return res.status(404).send('Course not found!')
+    }
+
+    const {error} = validateCoure(req.body)
+    if(error) {
+        res.status(400).send(error.details[0].message)
+        return
+    }
+
+    course.name = req.body.name
+    res.status(200).send(course)
+
+})
+
+app.delete('/api/courses/:id',(req,res) => {
+    
+    const course = courses.find(p => p.id === parseInt(req.params.id))
+    if(!course){
+        return res.status(404).send('Course not found!')
+    }
+
+    const index = courses.indexOf(course)
+    courses.splice(index, 1)
+    res.status(200).send(course)
+})
+
+function validateCoure(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course,schema)
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
